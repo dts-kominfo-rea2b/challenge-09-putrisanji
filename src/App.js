f47d2d31a77a14ea2277a82944cea7c1e90a0fc2
putri <putrisanjiwanidewi@gmail.com>
2022-06-26 16:29:26 +0900

    submitt

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import './App.css';
+import "./App.css";
 
 // Uncomment untuk memuat daftar kontak
 // import contacts from './data/contacts.json';
+import Header from "./components/Header";
+import Contact from "./components/Contact";
+import contacts from "./data/contacts.json";
 
 const App = () => {
-  // Masukkan Header dan Contact ke dalam div App
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+    // Masukkan Header dan Contact ke dalam div App
+
+    return (
+        <div className="App">
+            <Header />
+            {contacts?.map((contact) => (
+                <Contact data={contact} />
+            ))}
+        </div>
+    );
+};
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,23 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import "./Contact.css";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data }) => {
+    {
+        return (
+            <div className="container">
+                <div className="card">
+                    <img className="img" src={data.photo} alt={data.name} />
+                    <div className="content">
+                        <h2>{data.name}</h2>
+                        <p>{data.phone}</p>
+                        <p>{data.email}</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+};
 
 export default Contact;
